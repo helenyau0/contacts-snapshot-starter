3742d1791c10bd077dbcd5672e00465a4ca8def8
Helen Yau <helenyau13@gmail.com>
2017-09-19 17:21:52 -0700

    create test to test the createContact function

--- test/integration/db/contacts.test.js
+++ test/integration/db/contacts.test.js
@@ -11,5 +11,22 @@ describe('contacts', () => {
     it('should be a function', () => {
       expect(contacts.createContact).to.be.a('function')
     })
+    it.only('should insert a contact by first name and last name', (done) => {
+
+      const contact = {
+        first_name: 'chococat',
+        last_name: 'kitty'
+      }
+      contacts.createContact(contact)
+      .then(contact => {
+        expect(contact).to.be.an('array')
+        expect(contact[0]).to.be.an('object')
+        expect(typeof contact[0].id).to.equal('number')
+        expect(contact[0].first_name).to.deep.equal('chococat')
+        expect(contact[0].last_name).to.deep.equal('kitty')
+        done()
+      })
+      .catch(err => done(err))
+    })
   })
 })
